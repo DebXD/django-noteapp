ddfc21a1fc874e2f0743755b5aee914ab77e7bf7
DebiprasadXD <debiprasaddaskdv@gmail.com>
2023-01-17 14:08:46 +0530

    add requirements

--- notemy/settings.py
+++ notemy/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import psycopg2
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+#ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -86,25 +86,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('DATABASE'),
-        'USER': 'notemydj_willwidely',
+        'USER': config('USER'),
         'PASSWORD': config('PASSWORD'),
         'HOST': config('HOST'),
         'PORT': config('PORT'),
   }
     }
 
-
-
-#DATABASES = {
-    #'default': {
-      #  'ENGINE': 'django.db.backends.postgresql_psycopg2',
-       #'NAME': 'xgbmsaze',
-       # 'USER': 'xgbmsaze',
-       # 'PASSWORD': 'ygMUF8WqbnjDlzAt36Y2c4Ka-Y2rdFSl',
-       # 'HOST':'jelani.db.elephantsql.com',
-       # 'PORT': '5432'
-  # }
-#}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
